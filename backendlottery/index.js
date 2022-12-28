164bb941f3ded2b04fce5658fe0943b73fbe66b5
visakhpootheri <visakhpootheri97@gmail.com>
2022-12-28 15:53:39 +0530

    passwordedit

--- backendlottery/index.js
+++ backendlottery/index.js
@@ -50,60 +50,26 @@ app.post("/userlotterycount",(req,res)=>{
   })
 })
 
-//--------------users details of a specific lottery--------------//
-
-app.post("/lotterypurchasedusers",(req,res)=>{
-  let lottery=req.body.lotteryname;
-  var sql="SELECT TUR.txtFname,TUR.txtLname,date_format(TU.txtPurchaseddate,'%Y-%m-%d'),date_format(TLM.dtLotterydrawdate,'%Y-%m-%d') from tblunit TU join tbllotterymaster TLM on TU.refLotterymaster=TLM.id join tblusers TUR on TU.refUser=TUR.id where TLM.txtLotteryname='"+lottery+"';"
+//-----------userpasswordedit-------------//
+app.post("/updatepassword",(req,res)=>{
+  let userid=req.body.userid;
+  let newpassword=req.body.password;
+  var sql="update tblusers set txtUpassword='"+newpassword+"' where tblusers.id='"+userid+"';"
   con.query(sql,function(err,result){
     if(err) throw err;
+    console.log(result);
     res.send(result);
-  })
-})
-
-//-------user details who purchased a specific lottery,date------------//
-app.post("/userlist",(req,res)=>{
-  let Purchaseddate=req.body.purchaseddate;
-  let lottery=req.body.lotteryname;
-  var sql="select TUR.id,TUR.txtFname,TUR.txtLname FROM tblusers TUR JOIN tblunit TU ON TUR.id=TU.refUser join tbllotterymaster TLM on TU.refLotterymaster=TLM.id where TLM.txtLotteryname='"+lottery+"' && TU.txtPurchaseddate='"+Purchaseddate+"';"
+  });
+});
+app.post("/showpassword",(req,res)=>{
+  let userid=req.body.userid;
+  var sql="select TU.id,TU.txtUpassword from tblusers TU where  TU.id='"+userid+"';"
   con.query(sql,function(err,result){
     if(err) throw err;
+    console.log(result);
     res.send(result);
-  })
-})
-//--------------loterylist using drawdate---//
-app.post("/lotterylist",(req,res)=>{
-  let Drawdate=req.body.drawdate;
-  var sql="select txtLotteryname from tbllotterymaster where dtLotterydrawdate='"+Drawdate+"';"
-  con.query(sql,function(err,result){
-    if(err) throw err;
-    res.send(result);
-  })
-})
-
-//------------CHOISE LIST-----------//
-
-app.post("/choicelist",(req,res)=>{
-  let lottery=req.body.lotteryname;
-  var sql="select TU.txtFirstchoicenumber,TU.txtSecondchoicenumber,TU.txtThirdchoicenumber,TU.txtFourthchoicenumber,TU.txtFifthoicenumber from tblunit TU join tbllotterymaster TLM on TU.refLotterymaster=TLM.id where TLM.txtLotteryname='"+lottery+"';"
-  con.query(sql,function(err,result){
-    if(err) throw err;
-    res.send(result);
-  })
-})
-
-//----------------for select all-----------//
-
-app.post("/selectall",(req,res)=>{
-  let lottery=req.body.lotteryname;
-  let Purchaseddate=req.body.purchaseddate;
-  let Drawdate=req.body.drawdate;
-  var sql="select TUR.id,TUR.txtFname,TUR.txtLname from tblusers TUR join tblunit TU on TUR.id=TU.refUser join tbllotterymaster TLM on TU.refLotterymaster=TLM.id where TLM.txtLotteryname='"+lottery+"' && TLM.dtLotterydrawdate='"+Drawdate+"' && TU.txtPurchaseddate='"+Purchaseddate+"';"
-  con.query(sql,function(err,result){
-    if(err) throw err;
-    res.send(result);
-  })
-})
+  });
+});
 
 app.listen(8080, () => {
   console.log("listening on port");
